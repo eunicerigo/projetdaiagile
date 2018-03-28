d60995acfb92be1c2c39f8f30a4768a783f67145
AndersenSergio <setilasergio@gmail.com>
2018-03-28 15:16:43 +0200

    METIER ET BD

--- src/main/java/metier/Utilisateur.java
+++ src/main/java/metier/Utilisateur.java
@@ -5,6 +5,7 @@
  */
 package metier;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -24,23 +25,26 @@ public class Utilisateur {
     private String typeu;
     private Date dateinscri;
     private Date datevalid;
+    private String statutu;
 
     /**
-     * Constructeur avec tout les éléments de l'utilisateur
-     * @param codeu
-     * @param nomu
-     * @param prenomu
-     * @param mailu
-     * @param mdpu
-     * @param genreu
-     * @param datenaissanceu
-     * @param telu
-     * @param infooptionnelle
-     * @param typeu
-     * @param dateinscri
-     * @param datevalid 
+     * Constructeur d'un utilisateur avec tout ces attributs
+     * @param codeu le code de l'utilisateur
+     * @param nomu le nom de l'utilisateur
+     * @param prenomu le prenom de l'utilisateur
+     * @param mailu l'email de l'utilisateur
+     * @param mdpu le mot de passe de l'utilisateur
+     * @param genreu le genre de l'utilisateur
+     * @param datenaissanceu la date de naissance de l'utilisateur
+     * @param telu le numéro de téléphone de l'utilisateur
+     * @param infooptionnelle les informations optionnelles sur l'utilisateur 
+     * @param typeu le type de l'utilisateur (Admin, Coach, Client)
+     * @param dateinscri la date à laquelle l'utilisateur s'est inscrit
+     * @param datevalid la date à laquelle l'inscription d'un utilisateur a été 
+     * validé
+     * @param statutu le statut de l'utilisateur (Potentiel, En attente, ...)
      */
-    public Utilisateur(int codeu, String nomu, String prenomu, String mailu, String mdpu, String genreu, Date datenaissanceu, String telu, String infooptionnelle, String typeu, Date dateinscri, Date datevalid) {
+    public Utilisateur(int codeu, String nomu, String prenomu, String mailu, String mdpu, String genreu, Date datenaissanceu, String telu, String infooptionnelle, String typeu, Date dateinscri, Date datevalid, String statutu) {
         this.codeu = codeu;
         this.nomu = nomu;
         this.prenomu = prenomu;
@@ -53,121 +57,258 @@ public class Utilisateur {
         this.typeu = typeu;
         this.dateinscri = dateinscri;
         this.datevalid = datevalid;
+        this.statutu = statutu;
     }
 
-    public Utilisateur(int codeu, String nomu, String prenomu, String mailu, String mdpu, String genreu, String telu) {
-        this.codeu = codeu;
+    /**
+     * Constructeur d'un utilisateur pour l'inscription de base
+     * @param nomu le nom de l'utilisateur
+     * @param prenomu le prenom de l'utilisateur
+     * @param mailu l'email de l'utilisateur
+     * @param mdpu le mot de passe de 'lutilisateur
+     * @param genreu le genre de l'utilisateur
+     * @param datenaissanceu la date de naissance de l'utilisateur
+     * @param telu le numéro de télaphone du client
+     * @param typeu le type de l'utilisateur
+     * @param dateinscri la date à laquele l'utilisateur s'est inscrit
+     * @param statutu le statut de l'utilisateur
+     */
+    public Utilisateur(String nomu, String prenomu, String mailu, String mdpu, 
+            String genreu, Date datenaissanceu, String telu, String typeu, 
+            Date dateinscri, String statutu) {
         this.nomu = nomu;
         this.prenomu = prenomu;
         this.mailu = mailu;
         this.mdpu = mdpu;
         this.genreu = genreu;
+        this.datenaissanceu = datenaissanceu;
         this.telu = telu;
+        this.typeu = typeu;
+        this.dateinscri = dateinscri;
+        this.statutu = statutu;
     }
 
-    public Utilisateur(String nomu, String prenomu) {
+    /**
+     * Constructeur pour l'utilisateur récupérer dans la liste
+     * @param nomu le nom d el'utilisateur
+     * @param prenomu le prenom de l'utilisateur
+     * @param statutu le statut de l'utilisateur
+     * @param dateinscri la date à laquele le client s'est inscrit
+     */
+    public Utilisateur(String nomu, String prenomu, String statutu, 
+            Date dateinscri) {
         this.nomu = nomu;
         this.prenomu = prenomu;
+        this.statutu = statutu;
+        this.dateinscri = dateinscri;
     }
-    
-    
 
+    /**
+     * Méthode permettant de récupérer le code de l'utilisateur
+     * @return le code de l'utilisateur
+     */
     public int getCodeu() {
         return codeu;
     }
 
-    public void setCodeu(int codeu) {
-        this.codeu = codeu;
-    }
-
+    /**
+     * Méthode permettant de récupérer le nom de l'utilisateur
+     * @return le nom de l'utilisateur
+     */
     public String getNomu() {
         return nomu;
     }
 
+    /**
+     * Procédure permettant de modifier le nom de l'utilisateur
+     * @param nomu le nouveau nom de l'utilisateur
+     */
     public void setNomu(String nomu) {
         this.nomu = nomu;
     }
 
+    /**
+     * Méthode permettant de récupérer le prénom de l'utilisateur
+     * @return le prénom d l'utilisateur
+     */
     public String getPrenomu() {
         return prenomu;
     }
 
+    /**
+     * Procédure permettant de modifier le prénom de l'utilisateur
+     * @param prenomu le nouveau prénom de l'utilisateur
+     */
     public void setPrenomu(String prenomu) {
         this.prenomu = prenomu;
     }
 
+    /**
+     * Méthode permettant de récupérer l'email de l'utilisateur
+     * @return l'email de l'utilisateur
+     */
     public String getMailu() {
         return mailu;
     }
 
+    /**
+     * Procédure permettant de modifier l'email de l'utilisateur
+     * @param mailu le nouvel email de l'utilisateur
+     */
     public void setMailu(String mailu) {
         this.mailu = mailu;
     }
 
+    /**
+     * Méthode permettant de récupérer le mot de passe de l'utilisateur
+     * @return le mot de passe de l'utilisateur
+     */
     public String getMdpu() {
         return mdpu;
     }
 
+    /**
+     * Procédure permettant de modifier le mot de passe d'un utilisateur
+     * @param mdpu le mot de passe de l'utilisateur
+     */
     public void setMdpu(String mdpu) {
         this.mdpu = mdpu;
     }
 
+    /**
+     * Méthode permettant de récupérer le genre de l'utilisateur
+     * @return le genre de l'utilisateur
+     */
     public String getGenreu() {
         return genreu;
     }
 
+    /**
+     * Procédure permettant de modifier le genre de l'utilisateur
+     * @param genreu le nouveau genre de l'utilisateur
+     */
     public void setGenreu(String genreu) {
         this.genreu = genreu;
     }
 
+    /**
+     * Méthode permettant de récupérer la date de naissance de l'utilisateur
+     * @return la date de naissance de l'utilisateur
+     */
     public Date getDatenaissanceu() {
         return datenaissanceu;
     }
 
+    /**
+     * Procédure peremattant de modifier la date de naissance de l'utilisateur
+     * @param datenaissanceu la nouvelle date de naissance de l'utilisateur
+     */
     public void setDatenaissanceu(Date datenaissanceu) {
         this.datenaissanceu = datenaissanceu;
     }
 
+    /**
+     * Procédure permettant de récupérer le numéro de téléphone de l'utilisateur
+     * @return le numéro de téléphone de l'utilisateur
+     */
     public String getTelu() {
         return telu;
     }
 
+    /**
+     * Procédure permettant de modifier le numéro de téléphone de l'utilisateur
+     * @param telu le nouveau numéro de téléphone de l'utilisateur
+     */
     public void setTelu(String telu) {
         this.telu = telu;
     }
 
+    /**
+     * Méthode permettant de récupérer le sinformations optionnelles de 
+     * l'utilisateur
+     * @return les informations optionnelles de l'utilisateur
+     */
     public String getInfooptionnelle() {
         return infooptionnelle;
     }
 
+    /**
+     * Procédure permettant de modifier les informations optionnelles de 
+     * l'utilisateur
+     * @param infooptionnelle les nouvelles informations optionnelles de 
+     * l'utilisateur
+     */
     public void setInfooptionnelle(String infooptionnelle) {
         this.infooptionnelle = infooptionnelle;
     }
 
+    /**
+     * Méthode permettat de récupérer le type de l'utilisateur
+     * @return le type de l'utilisateur (Client, Coach, Admin)
+     */
     public String getTypeu() {
         return typeu;
     }
 
-    public void setTypeu(String typeu) {
-        this.typeu = typeu;
-    }
-
+    /**
+     * Méthode permettant de récupérer la date d'inscription de l'utilisateur
+     * @return la date d'inscription de l'utilisateur
+     */
     public Date getDateinscri() {
         return dateinscri;
     }
-
+    
+    /**
+     * Procédure permettant de modifier la date d'inscrption de l'utilisateur
+     * @param dateinscri la nouvelle date d'inscription de l'utilisateur
+     */
     public void setDateinscri(Date dateinscri) {
         this.dateinscri = dateinscri;
     }
 
+    /**
+     * Méthode permettant de récupérer la date de validation de l'inscription 
+     * d'un utilisateur
+     * @return la date de validation de l'inscription
+     */
     public Date getDatevalid() {
         return datevalid;
     }
 
+    /**
+     * Procédure permettant de modifier la date de validation de l'inscription
+     * d'un utilisateur
+     * @param datevalid la nouvelle date de validation de l'inscription d'un 
+     * utilisateur
+     */
     public void setDatevalid(Date datevalid) {
         this.datevalid = datevalid;
     }
+
+    /**
+     * Méthode permettant de récupérer le statut d'un utilisateur
+     * @return le statut d'un utilisateur (Potentiel, En Attente, Abonné Actif,
+     * Abonné Inactif, Ancien Abonné)
+     */
+    public String getStatutu() {
+        return statutu;
+    }
+
+    /**
+     * Procédure permettant de modifier le statut d'un utilisateur
+     * @param statutu le nouveau statut d'un utilisateur
+     */
+    public void setStatutu(String statutu) {
+        this.statutu = statutu;
+    }
     
-    
+    /**
+     * Méthode permettant de récupérer la date de naissance au format String
+     * @return la date de naissance au format String
+     */
+    public String getStringDate(Date d) {
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy");
+        return formatDate.format(d);
+    }
     
 }
